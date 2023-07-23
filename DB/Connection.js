919ab68b97263bff8cff34cd8fadd1ef35a24dc8
Gustavo M. Cazarini <gustavo.cazarini@outlook.com>
2023-07-22 21:39:57 -0300

    Add: Databa connection

---
+++ DB/Connection.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: process.env.HOST,
+    user: process.env.USUARIO,
+    password: process.env.PASS,
+    port: process.env.DBPORT,
+    database: process.env.DATABASE,
+    ssl: {
+        require: JSON.parse(process.env.SSLREQ),
+        rejectUnauthorized: JSON.parse(process.env.SSLREJECT)
+    }
+});
+
+module.exports = pool;

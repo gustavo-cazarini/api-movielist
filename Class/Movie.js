131cd285e0e2e95b3ce2b35215bd03569b7a3e1a
Gustavo M. Cazarini <gustavo.cazarini@outlook.com>
2023-07-23 15:18:38 -0300

    refactor: movie class get method

--- Class/Movie.js
+++ Class/Movie.js
@@ -1,3 +1,5 @@
+const pool = require('../DB/Connection');
+
 class Movie {
     constructor(id, title, plot, year, runtime, imdbRating, poster) {
         this.id = id;
@@ -7,6 +9,35 @@ class Movie {
         this.runtime = runtime;
         this.imdbRating = imdbRating;
         this.poster = poster;
+    }
+
+    // Private methods
+    #selectQuery(field, id) {
+        // The 'field' param needs to be an array, that specifies
+        // which fields we need to bring from the database
+        let query = `SELECT `;
+        let fields = ``;
+
+        if (field?.length) {
+            field.forEach(value => {
+                fields += `${value}, `;
+            });
+            fields = fields.slice(0, -2) + fields.slice(-1);
+        } else fields += `* `;
+
+        query += fields + `FROM movie `;
+        if (id) query += `WHERE id = ${this.id}`
+
+        return query;
+    }
+
+    // Public methods
+    async get(field, id) {
+        const strQuery = this.#selectQuery(field, id);
+        const connection = await pool.connect();
+        const query = await connection.query(strQuery);
+
+        return query.rows;
     }
 }
 

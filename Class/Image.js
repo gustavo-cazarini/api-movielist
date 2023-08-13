f12328bad604a44826326a35b72cab024fc09942
Gustavo M. Cazarini <gustavo.cazarini@outlook.com>
2023-08-12 21:06:19 -0300

    add: post method in image class

--- Class/Image.js
+++ Class/Image.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const pool = require('../DB/Connection');
 
 // class Image extends Movie
@@ -5,7 +6,7 @@ class Image {
     constructor(id, url, movieId) {
         this.id = id;
         this.url = url;
-        this.movieId = movieId
+        this.movieId = movieId;
     }
 
     #getQuery(fields) {
@@ -24,11 +25,29 @@ class Image {
         return query;
     }
 
+    #postQuery() {
+        if (!this.url?.length && !this.movieId) return false;
+        let query = `INSERT INTO image (url, fk_movie_id) VALUES `;
+        this.url.forEach(fileurl => {
+            query += `('${fileurl}', ${this.movieId}), `;
+        });
+        query = query.slice(0, -2) + query.slice(-1);
+        return query;
+    }
+
     async getImage(fields) {
         const strQuery = this.#getQuery(fields);
         const connection = await pool.connect();
         const queryResult = await connection.query(strQuery);
         return queryResult.rows;
+    }
+
+    async postImage() {
+        const strQuery = this.#postQuery();
+        if (!strQuery) return false;
+        const connection = await pool.connect();
+        const queryResult = await connection.query(strQuery);
+        return queryResult;
     }
 }
 
